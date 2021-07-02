d1ebe9ec32d78be4abe272cc38c4b3a744a71a89
Andreas Rozek <A.Rozek@gmx.de>
2021-07-02 12:49:08 +0200

    Update save-to-file.ts

--- src/save-to-file.ts
+++ src/save-to-file.ts
@@ -3,8 +3,8 @@
 //----------------------------------------------------------------------------//
 // see https://stackoverflow.com/questions/68213608/how-to-send-the-result-of-svelte-preprocess-into-a-file
 
-  import fs   from 'fs'
-  import path from 'path'
+  const fs   = require('fs')
+  const path = require('path')
 
   import {
     throwError,
@@ -36,240 +36,6 @@
   }
 
 
-//----------------------------------------------------------------------------//
-//                                HTML Parsing                                //
-//----------------------------------------------------------------------------//
-
-  type HTMLAttribute = {
-    Name:string,
-    Value:string,
-    escapedValue:string
-  }
-
-  type HTMLParserCallbackSet = {
-    processStartTag ?: (TagName:string, Attributes:HTMLAttribute[], isUnary:boolean, isTopLevel:boolean) => any,
-    processEndTag   ?: (TagName:string, isTopLevel:boolean) => any,
-    processText     ?:    (Text:string, isTopLevel:boolean) => any,
-    processComment  ?: (Comment:string) => any
-  }
-
-  type TagStack = string[] & { last:() => string|undefined }
-
-  function parseHTML (HTML:string, Callbacks:HTMLParserCallbackSet):void {
-    const StartTagPattern  = /^<([-a-z0-9]+)((?:[\s\xA0]+[-a-z0-9_]+(?:[\s\xA0]*=[\s\xA0]*(?:(?:"[^"]*")|(?:'[^']*')|[^>\s\xA0]+))?)*)[\s\xA0]*(\/?)>/i
-    const EndTagPattern    = /^<\/([-a-z0-9_]+)[^>]*>/i
-    const AttributePattern = /([-a-z0-9]+)(?:[\s\xA0]*=[\s\xA0]*(?:(?:"((?:\\.|[^"])*)")|(?:'((?:\\.|[^'])*)')|([^>\s\xA0]+)))?/gi
-
-  /**** MapOf makes a "map" with keys from a given comma-separated key list ****/
-
-    function MapOf (Elements:string):{ [Key:string]:boolean } {
-      let ElementList = Elements.split(',')
-      let Result = Object.create(null)
-        for (let i = 0, l = ElementList.length; i < l; i++) {
-          Result[ElementList[i]] = true
-        }
-      return Result
-    }
-
-  /**** maps with the names of tags and attributes with a specific characteristic ****/
-
-    const isEmptyElement = MapOf(
-      'area,base,basefont,br,col,embed,frame,hr,img,input,isIndex,keygen,link,' +
-      'meta,param,source,track,wbr'
-    )
-
-    const isBlockElement = MapOf(
-      'address,article,aside,audio,blockquote,canvas,center,dd,dir,div,dl,dt,' +
-      'fieldset,figcaption,figure,footer,form,frameset,h1,h2,h3,h4,h5,h6,header,' +
-      'hgroup,hr,ins,isIndex,li,main,menu,nav,noframes,noscript,ol,output,p,pre,' +
-      'section,table,tbody,td,tfoot,th,thead,tr,ul,video' +
-      ',applet' // added for WAT
-    )
-
-    const isInlineElement = MapOf(
-      'a,abbr,acronym,Applet,b,basefont,bdo,big,br,button,cite,code,del,dfn,em,' +
-      'font,i,iframe,img,input,ins,kbd,label,map,Object,q,s,samp,Script,select,' +
-      'small,span,strike,strong,sub,sup,textarea,tt,u,var'
-    )
-
-    const isSelfClosingElement = MapOf(
-      'area,base,basefont,bgsound,br,col,colgroup,dd,dt,embed,frame,hr,img,' +
-      'input,isIndex,keygen,li,link,menuitem,meta,options,p,param,source,td,' +
-      'tfoot,th,thead,tr,track,wbr'
-    )
-
-    const isSpecialElement = MapOf(
-      'script,style'
-    )
-
-    const isKeywordAttribute = MapOf(
-      'checked,compact,declare,defer,disabled,ismap,multiple,nohref,noresize,' +
-      'noshade,nowrap,readonly,selected'
-    )
-
-  /**** actual HTML parser ****/
-
-    let doNothing = function ():void {}
-
-    let processStartTag = Callbacks.processStartTag || doNothing
-    let processEndTag   = Callbacks.processEndTag   || doNothing
-    let processText     = Callbacks.processText     || doNothing
-    let processComment  = Callbacks.processComment  || doNothing
-
-// @ts-ignore "Stack.last" will be defined just one line later
-    let Stack:TagStack = []    // Stack with tag names of unclosed HTML elements
-      Stack.last = function last ():string|undefined { return this[this.length-1] }
-
-    function parseStartTag (
-      _:any, TagName:string, rest:string, isUnary?:boolean
-    ):string {
-      TagName = TagName.toLowerCase()
-
-      if (isBlockElement[TagName]) {            // close pending inline elements
-        while ((Stack.last() != null) && isInlineElement[Stack.last() as string]) {
-          parseEndTag('', Stack.last())
-        }
-      }
-
-      if (isSelfClosingElement[TagName] && (Stack.last() === TagName)) {
-        parseEndTag('', TagName)
-      }
-
-      isUnary = isEmptyElement[TagName] || !!isUnary
-      if (! isUnary) { Stack.push(TagName) }
-
-      if (processStartTag !== doNothing) {
-        let Attributes:any[] = []
-          rest.replace(AttributePattern, function(Match:any, AttributeName:string):string {
-            let AttributeValue:string = (arguments[2] ? arguments[2] :
-              arguments[3] ? arguments[3] :
-                arguments[4] ? arguments[4] :
-                  isKeywordAttribute[AttributeName] ? AttributeName : '')
-            Attributes.push({
-              Name:AttributeName, Value:AttributeValue,
-              escapedValue:AttributeValue.replace(/(^|[^\\])"/g, '$1\\\"')
-            })
-            return ''
-          })
-        processStartTag(
-          TagName, Attributes, isUnary, (Stack.length === (isUnary ? 0 : 1))
-        )
-      }
-
-      return ''
-    }
-
-    function parseEndTag (_?:any, TagName?:string):string {
-      let Position;                 // how many open elements have to be closed?
-      if (TagName == null) {
-        Position = 0
-      } else {
-        TagName = TagName.toLowerCase()
-
-        for (Position = Stack.length-1; Position >= 0; Position--) {
-          if (Stack[Position] === TagName) { break }
-        }
-      }
-
-      if (Position >= 0) {
-        for (let i = Stack.length-1; i >= Position; i--) {
-          processEndTag(Stack[i], (i === 0))
-        }
-        Stack.length = Position
-      }
-
-      return ''
-    }
-
-    let lastHTMLContent = HTML
-    while (HTML !== '') {
-      let inText = true
-
-      if ((Stack.last() == null) || ! isSpecialElement[Stack.last() as string]) {
-        if (HTML.startsWith('<!--')) {                           // HTML comment
-          let Index = HTML.indexOf('-->',4)
-          if (Index > 0) {
-            processComment(HTML.slice(4,Index))
-            HTML    = HTML.slice(Index + 3)
-            inText = false
-          }
-        } else if (HTML.startsWith('</')) {                      // HTML end tag
-          let Match = HTML.match(EndTagPattern)
-          if (Match != null) {
-            HTML = HTML.slice(Match[0].length)
-            Match[0].replace(EndTagPattern, parseEndTag);// for side effects
-            inText = false
-          }
-        } else if (HTML.startsWith('<')) {                     // HTML start tag
-          let Match = HTML.match(StartTagPattern)
-          if (Match != null) {
-            HTML = HTML.slice(Match[0].length)
-            Match[0].replace(StartTagPattern, parseStartTag);// for side effects
-            inText = false
-          }
-        }
-
-        if (inText) {
-          let Index = HTML.indexOf('<')
-
-          let Text = (Index < 0 ? HTML : HTML.slice(0,Index))
-          HTML     = (Index < 0 ? ''   : HTML.slice(Index))
-
-          processText(Text, Stack.length === 0)
-        }
-      } else {
-        HTML = HTML.replace(
-          new RegExp('^((?:.|\n)*?)<\/' + Stack.last() + '[^>]*>','i'),
-          function (_:any, Text:string):string {
-            Text = Text
-              .replace(/<!--(.*?)-->/g, '$1')
-              .replace(/<!\[CDATA\[(.*?)]]>/g, '$1')
-            processText(Text, Stack.length === 0)
-            return ''
-          }
-        )
-        parseEndTag('', Stack.last())
-      }
-
-      if (HTML === lastHTMLContent) {
-        switch (true) {
-          case HTML.startsWith('<'):
-            HTML = HTML.slice(1)
-            processText('&lt;', Stack.length === 0)
-            break
-          default:
-            throwError('HTMLParseError: could not parse "' + HTML + '"')
-        }
-      }
-      lastHTMLContent = HTML
-    }
-    parseEndTag()
-  }
-
-/**** AttributeFrom ****/
-
-  function AttributeFrom (AttributeName:string, Attributes:any[]):string|undefined {
-    for (let i = 0, l = Attributes.length; i < l; i++) {
-      let Attribute = Attributes[i]
-      if (Attribute.Name === AttributeName) { return Attribute.Value }
-    }
-    return undefined
-  }
-
-/**** deserializedTag ****/
-
-  function deserializedTag (
-    TagName:string, Attributes:HTMLAttribute[], isUnary:boolean
-  ):string {
-    let Result = '<' + TagName
-      for (let i = 0, l = Attributes.length; i < l; i++) {
-        let Attribute = Attributes[i]
-        Result += ' ' + Attribute.Name + '="' + Attribute.escapedValue + '"'
-      }
-    return Result + (isUnary ? '/>' : '>')
-  }
-
-
 /**** escapedHTMLAttribute ****/
 
   function escapedHTMLAttribute (OriginalValue:string):string {
@@ -297,9 +63,29 @@
   function saveToFile (FileName:string):Preprocessor {
     expectNonEmptyString('name of the file to save to',FileName)
 
-    let processedScript:string = ''
     let processedMarkup:string = ''
+    let processedScript:string = ''
     let processedStyle:string  = ''
+
+  /**** processMarkup ****/
+
+    const StylePattern  =  /\s*<style(\s[^>]*)?>[\s\S]*?<\/style>/gi  // not perfect but
+    const ScriptPattern = /\s*<script(\s[^>]*)?>[\s\S]*?<\/script>/gi // saves a parser
+
+    function processMarkup (input: { content:string }):PreprocessorResult {
+      let { content } = input
+
+      if (ValueIsNonEmptyString(content)) {
+        processedMarkup += content.replace(StylePattern,'').replace(ScriptPattern,'')
+
+        if (ValueIsEmptyString(processedMarkup)) {
+          processedMarkup = ''
+        }
+      }
+
+      return Promise.resolve({ code:content })
+    }
+
 
   /**** processScript ****/
 
@@ -309,7 +95,7 @@
       let { attributes,content } = input
 
       if (ValueIsNonEmptyString(content)) {
-        processedScript = '<script'
+        processedScript += '<script'
           for (let Key in attributes) {
             if (attributes.hasOwnProperty(Key) && (Key !== 'lang')) {
               processedScript += (
@@ -320,46 +106,8 @@
         processedScript += '>\n'
           content = content.replace(/^\s*\n/,'').replace(/\n\s*$/,'')
           processedScript += content     // w/o leading and trailing empty lines
-        processedScript += '\n</script>'
+        processedScript += '\n</script>\n'
       }
-      return Promise.resolve({ code:content })
-    }
-
-
-  /**** processMarkup ****/
-
-    function processMarkup (input: { content:string }):PreprocessorResult {
-      let { content } = input
-
-      if (ValueIsNonEmptyString(content)) {
-        let HTMLParserCallbacks = {
-          processStartTag: function processStartTag (
-            TagName:string, Attributes:HTMLAttribute[], isUnary:boolean, isTopLevel:boolean
-          ) {
-            if (! isTopLevel || ((TagName !== 'style') && (TagName !== 'script'))) {
-              processedMarkup += deserializedTag(TagName,Attributes,isUnary)
-            }
-          },
-          processEndTag: function processEndTag (TagName:string, isTopLevel:boolean) {
-            if (! isTopLevel || ((TagName !== 'style') && (TagName !== 'script'))) {
-              processedMarkup += '</' + TagName + '>'
-            }
-          },
-          processText: function processText (Text:string, isTopLevel:boolean) {
-            processedMarkup += Text
-          },
-          processComment: function processComment (Comment:string) {
-            processedMarkup += Comment
-          }
-        }
-
-        parseHTML(content, HTMLParserCallbacks)
-
-        if (ValueIsEmptyString(processedMarkup)) {
-          processedMarkup = ''
-        }
-      }
-
       return Promise.resolve({ code:content })
     }
 
@@ -372,7 +120,7 @@
       let { attributes,content } = input
 
       if (ValueIsNonEmptyString(content)) {
-        processedStyle = '<style'
+        processedStyle += '<style'
           for (let Key in attributes) {
             if (attributes.hasOwnProperty(Key) && (Key !== 'lang')) {
               processedStyle += (
@@ -383,16 +131,18 @@
         processedStyle += '>\n'
           content = content.replace(/^\s*\n/,'').replace(/\n\s*$/,'')
           processedStyle += content      // w/o leading and trailing empty lines
-        processedStyle += '\n</style>'
+        processedStyle += '\n</style>\n'
       }
 
-    /**** now it's time to write anything into the given file ****>
+    /**** now it's time to write anything into the given file ****/
 
-      let completeFilename = path.join(process.cwd(),FileName)
-      fs.writeFileSync(completeFilename, (
-        (processedStyle  === '' ? '' : processedStyle  + '\n') +
-        (processedScript === '' ? '' : processedScript + '\n') +
-        (processedMarkup === '' ? '' : processedMarkup + '\n')
+      let completeFileName = path.join(process.cwd(),FileName)
+
+      fs.mkdirSync(path.dirname(completeFileName),{ recursive:true })
+      fs.writeFileSync(completeFileName, (
+        (processedMarkup === '' ? '' : processedMarkup) +
+        (processedScript === '' ? '' : processedScript) +
+        (processedStyle  === '' ? '' : processedStyle)
       ))
 
     /**** that's it! ****/
