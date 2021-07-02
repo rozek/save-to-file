8ef5a5f387809786af66d6e7902cb753e5472eb7
Andreas Rozek <A.Rozek@gmx.de>
2021-07-02 09:58:53 +0200

    create build environment

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+// see https://remarkablemark.org/blog/2019/07/12/rollup-commonjs-umd/
+
+import commonjs       from '@rollup/plugin-commonjs'
+import resolve        from '@rollup/plugin-node-resolve'
+import autoPreprocess from 'svelte-preprocess'
+import typescript     from '@rollup/plugin-typescript'
+import { terser }     from 'rollup-plugin-terser'
+
+export default {
+  input: './src/save-to-file.ts',
+  output: [
+    {
+      file:     './dist/save-to-file.esm.js',
+      format:   'esm',
+      sourcemap:true,
+    }
+  ],
+  plugins: [
+    resolve(), commonjs(), typescript(),
+    terser({ format:{ comments:false } }),
+  ],
+};
